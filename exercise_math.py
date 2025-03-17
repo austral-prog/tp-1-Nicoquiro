79272038c63bf12a0c09e3a05c83b58b5e66290e
Nicoquiro <165960676+Nicoquiro@users.noreply.github.com>
2025-03-17 11:59:40 -0300

    Update exercise_math.py
    Entrega del trabajo 1 ejercicio 2

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,23 @@
 def math():
     a = 57
     b = 7
+    suma= a + b   
+    print(suma) 
+
+    resta= a - b
+    print(resta)
+
+    multiplicacion= a * b
+    print(multiplicacion)
+
+    Promedio = (a + b) / 2
+    print(Promedio)
+
+    cociente_entero= a//b
+    print(cociente_entero)
+
+    resto_division= a%b
+    print(resto_division)
+
+    division_real= a / b
+    print(division_real)
